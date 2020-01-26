a01aa4190259f85a62d95757e50665929e235587
Michal R <marelix2@gmail.com>
2020-01-26 17:43:42 +0100

    4: repertory view complete

---
+++ src/static/movies/movies.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id:'1',
+    title: 'Bad Boys For Life',
+    production: 'USA',
+    type: ['Akcja', 'Kryminał'],
+    timespan: '124 min',
+    img: 'BadBoysForLife.jpg'
+  },
+  {
+    id:'2',
+    title: 'Jojo Rabbit',
+    production: 'Niemcy/USA',
+    type: ['Wojenny', 'Komediodramat'],
+    timespan: '108 min.',
+    img: 'JojoRabbit.jpg'
+  },
+  { 
+    id:'3',
+    title: 'Doktor Dolittle',
+    production: 'USA',
+    type: ['Komedia', 'Przygodowy'],
+    timespan: '102 min',
+    img: 'DoktorDolittle.jpg'
+  },
+  {
+    id:'4',
+    title: 'Mayday',
+    production: 'Polska',
+    type: ['Komedia'],
+    timespan: '113 min',
+    img: 'Mayday.jpg'
+  },
+  {
+    id:'5',
+    title: 'Gorący temat',
+    production: 'USA',
+    type: ['Akcja', 'Kryminał'],
+    timespan: '124 min',
+    img: 'GoracyTemat.jpg'
+  },
+  {
+    id:'6',
+    title: '1917',
+    production: 'Wlk. Brytania/USA',
+    type: ['Dramat', 'Wojenny'],
+    timespan: '119 min',
+    img: '1917.jpg'
+  },
+  {
+    id:'7',
+    title: 'Urwis',
+    production: 'Kanada/USA/Kajmany',
+    type: ['Animacja'],
+    timespan: '88 min',
+    img: 'Urwis.jpg'
+  },
+]
--- src/view/Repertory/Repertory.jsx
+++ src/view/Repertory/Repertory.jsx
@@ -1,11 +1,48 @@
 import React, { useContext } from 'react'
 import DataContext from '../../context/context'
+import { makeStyles } from '@material-ui/styles'
+import { Grid, Typography } from '@material-ui/core'
+import MovieTile from './MovieTile'
+
+const useStyles = makeStyles(({ breakpoints }) => ({
+  container: {
+    [breakpoints.down('sm')]: {
+      flexDirection: 'column'
+    },
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  item: {
+    margin: 10
+  }
+}
+)
+)
+
 
 const Repertory = () => {
-  const { data } = useContext(DataContext)
+  const { movies } = useContext(DataContext)
+  const classes = useStyles()
+
   return (
     <>
-      witaj świecie
+      <Typography gutterBottom variant="h3" component="h2" align='center'>
+        Repertuar
+      </Typography>
+      <Grid container className={classes.container}>
+        {movies.map((movie) => (
+          <Grid
+            item
+            key={movie.id}
+            className={classes.item}
+          >
+            <MovieTile
+              movie={movie}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 };
